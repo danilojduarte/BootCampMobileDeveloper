ecfaa0c32c34dfce37a2493c47cb0a3c7b87718a
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-07-23 15:45:51 -0300

    Sobre conceitos de 'StyleSheet'

--- 03-ReactNative/01-IntroducaoReactNative/NovoProjetoExpo/green-lantern/App.js
+++ 03-ReactNative/01-IntroducaoReactNative/NovoProjetoExpo/green-lantern/App.js
@@ -4,16 +4,23 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{ color: 'red' }}>Hello Word!</Text>
-      <Text>Hello Word!</Text>
+      <Text style={styles2.titleStyle}>Hello Word!</Text>
       <Text style={{ 
         color: 'white', 
-        ackgroundColor: 'black',
+        backgroundColor: 'black',
         fontSize: 30, 
         }}
         >Iniciado meu BootCamp Dio de React Native!</Text>
     </View>
   );
 }
+
+const styles2 = StyleSheet.create({
+  titleStyle:{
+    fontSize: 15,
+    color: '#f1f1f1',
+  }
+})
 
 const styles = StyleSheet.create({
   container: {
