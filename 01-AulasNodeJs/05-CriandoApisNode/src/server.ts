d43f75772d323d5996e15efbc3d9aa7cc89b8bd2
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-07-28 14:53:39 -0300

    Criando servidor utilizando http
    //Finalizada aula Server Https na prática

--- 01-AulasNodeJs/05-CriandoApisNode/src/server.ts
+++ 01-AulasNodeJs/05-CriandoApisNode/src/server.ts
@@ -1,1 +1,9 @@
-console.log("Iniciando o servidor...");
+import * as http from "http";
+
+const server = http.createServer((request: http.IncomingMessage, response: http.ServerResponse) => {});
+
+const port = process.env.PORT;
+
+server.listen(process.env.PORT, () => {
+  console.log(`Servidor iniciado na porta ${port}`);
+});
