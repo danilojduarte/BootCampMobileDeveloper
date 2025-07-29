3bbdb57bb7b2999a19c139964be7a905d08b22ca
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-07-29 15:15:40 -0300

    Iniciando Aula, A service

--- 01-AulasNodeJs/05-CriandoApisNode/src/controllers/podcasts-controller.ts
+++ 01-AulasNodeJs/05-CriandoApisNode/src/controllers/podcasts-controller.ts
@@ -11,16 +11,12 @@ export const getListEpisodes = async (
         podcastName: "flow",
         episodio: "ALOK - Flow #319",
         videoId: "XhQuJI8S55c",
-        cover: "https://i.ytimg.com/vi/XhQuJI8S55c/maxresdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDSDaw428aLerjMRrz3iU1Z4mI4xg",
-        link: "https://www.youtube.com/watch?v=XhQuJI8S55c",
         category: ["música", "show"]
 },
 {
         podcastName: "flow",
         episodio: "SERGIO - Flow #320",
         videoId: "1ZLqvRvrg",
-        cover: "https://i.ytimg.com/vi/g-1ZLqvRvrg/maxresdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCoHQOY0UNaJRAC5t7kbJse3LTiig",
-        link: "https://www.youtube.com/watch?v=g-1ZLqvRvrg",
         category: ["ciência", "IA"],
 
       }, 
