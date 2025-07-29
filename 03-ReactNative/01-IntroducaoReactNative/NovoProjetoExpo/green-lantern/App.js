ed0b067538befc909500446e304dba90f875138b
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-07-29 16:42:46 -0300

    Introdução React Native
    Primeiros Passos com React Native & Expo

--- 03-ReactNative/01-IntroducaoReactNative/NovoProjetoExpo/green-lantern/App.js
+++ 03-ReactNative/01-IntroducaoReactNative/NovoProjetoExpo/green-lantern/App.js
@@ -1,26 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import symbolOn from './assets/pictures/symbol-on.png';
+import symbolOff from './assets/pictures/symbol-off.png';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: 'red' }}>Hello Word!</Text>
-      <Text style={styles2.titleStyle}>Hello Word!</Text>
-      <Text style={{ 
-        color: 'white', 
-        backgroundColor: 'black',
-        fontSize: 30, 
-        }}
-        >Iniciado meu BootCamp Dio de React Native!</Text>
+      <Image source={symbolOn}
+      ></Image>
     </View>
   );
 }
-
-const styles2 = StyleSheet.create({
-  titleStyle:{
-    fontSize: 15,
-    color: '#f1f1f1',
-  }
-})
 
 const styles = StyleSheet.create({
   container: {
