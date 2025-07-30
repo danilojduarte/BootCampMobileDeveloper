dad7d911a4dd2dcbf35d21039e6dc696918f58fa
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-07-30 14:54:22 -0300

    Aula Introducao React Native
    {Aula concluída e projeto finalizado

--- 03-ReactNative/01-IntroducaoReactNative/NovoProjetoExpo/green-lantern/App.js
+++ 03-ReactNative/01-IntroducaoReactNative/NovoProjetoExpo/green-lantern/App.js
@@ -1,22 +1,39 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import symbolOn from './assets/pictures/symbol-on.png';
 import symbolOff from './assets/pictures/symbol-off.png';
 
 
+
+
 export default function App() {
+  const [isActive, setIsActive] = useState(false);
+  
+  function handleSymbol(){
+    setIsActive(!isActive);
+  }
+
   return (
-    <View style={styles.container}>
-      <Image source={symbolOn}
-      ></Image>
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      <TouchableOpacity onPress={handleSymbol}>
+        <Image source={isActive ? symbolOn : symbolOff}></Image>
+      </TouchableOpacity>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOn: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerOff:{
+    flex: 1,
+    backgroundColor: 'darkgreen',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
