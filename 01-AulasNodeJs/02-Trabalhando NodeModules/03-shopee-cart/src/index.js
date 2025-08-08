822011374264bfa822db92258df9e736efdc8656
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-08-08 15:44:39 -0300

    Criando function para remover item

--- 01-AulasNodeJs/02-Trabalhando NodeModules/03-shopee-cart/src/index.js
+++ 01-AulasNodeJs/02-Trabalhando NodeModules/03-shopee-cart/src/index.js
@@ -1,10 +1,16 @@
+import * as cartService from './services/cart.js';
 import createItem from './services/item.js';
 
-const cart = [];
+const myCart = [];
+const myWhishiList = [];
 
 console.log("Welcome to the your Shopee Cart!");
 
 const item1 = await createItem("hotwheels ferrari", 20.99, 1);
 const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
 
-console.log(item2.subtotal())
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+await cartService.displaycart(myCart);
+
+await cartService.calculateTotal(myCart);
--- 01-AulasNodeJs/02-Trabalhando NodeModules/03-shopee-cart/src/services/cart.js
+++ 01-AulasNodeJs/02-Trabalhando NodeModules/03-shopee-cart/src/services/cart.js
@@ -1,17 +1,45 @@
 //quais ações meu carrinho pode fazer.
 //Add itens
 async function addItem(userCart, item) {
-  useCart.push(item);
+  userCart.push(item);
 }
-//Delete itens
-async function deleteItem(userCart, name) {
 
-}
-//Remover um item
-async function removeItem(userCart, index){
-
-}
 //Calcular o total
 async function calculateTotal(userCart) {
-  
+  console.log("\nShopee Cart Total Is:");
+
+  const result = userCart.reduce((total, item) => total  + item.subtotal(), 0);
+
+  console.log(`\🤑 Total: ${result}`);
 }
+
+//Delete itens
+async function deleteItem(userCart, name) {
+  const index = userCart.findIndex(item => item.name === name);
+  if (index !== -1) {
+    userCart.splice(index, 1);
+  } else {
+    console.log(`Item ${name} not found in the cart.`);
+  }
+}
+
+//Remover um item
+async function removeItem(userCart, index){
+  if (index >= 0 && index < userCart.length) {
+    userCart.splice(index, 1);
+  } else {
+    console.log(`Item at index ${index} not found in the cart.`);
+  }
+}
+
+async function displaycart(userCart){
+  console.log("Shopee cart list:");
+  userCart.forEach((item, index) => {
+    console.log(`${index + 1}. ${item.name} - R$ ${item.price} | 
+      ${item.quantity} x | Subtotal = ${item.subtotal()}`);
+  })
+}
+
+
+export {
+  addItem, calculateTotal, deleteItem, removeItem, displaycart};
