22ffa76fe566c91a806d21fc3314f674243613e6
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-08-23 15:20:08 -0300

    Ajustando api utilizando Filter Driver

--- 01-AulasNodeJs/05-CriandoApisNode/002-Projeto-Formula1/project-formula1/src/server.ts
+++ 01-AulasNodeJs/05-CriandoApisNode/002-Projeto-Formula1/project-formula1/src/server.ts
@@ -1,16 +1,30 @@
 import fastify from "fastify";
+import { request } from "http";
 
 const server = fastify({ logger: true });
+
+const teams = [
+  { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
+  { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
+  { id: 3, name: "Ferrari", base: "Maranello, Italy" },
+];
+
+const drivers = [
+  { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
+  { id: 2, name: "Lewis Hamilton", team: "Mercedes" },
+  { id: 3, name: "Charles Leclerc", team: "Ferrari" },
+];
 
 server.get("/teams", async(request, response) => {
   response.type("application/json").code(200)
 
-  return [
-    {id: 1, name: "ferrari"}
-  ];
+  return { teams };
 });
 
-
+server.get("/drivers", async(request, response) => {
+    response.type("application/json").code(200)
+    return { drivers };
+});
 
 server.listen({port: 3333}, () => {
   console.log("Server init");
