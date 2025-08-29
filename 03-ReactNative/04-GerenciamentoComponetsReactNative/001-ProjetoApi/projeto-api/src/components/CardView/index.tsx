35fe6b68fc54f3a5dc01c0a03e49392c63ea8d6d
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2025-08-29 04:31:03 -0300

    Aula, importando imagens

---
+++ 03-ReactNative/04-GerenciamentoComponetsReactNative/001-ProjetoApi/projeto-api/src/components/CardView/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Button, Image } from 'react-native';
+import { styles } from './styles';
+
+export default function CardView(){
+  return (
+    <View style={styles.imageContainer}>
+      <Text>CardView Works!!</Text>
+    </View>
+  );
+}
+
+// Aula, importando imagens
